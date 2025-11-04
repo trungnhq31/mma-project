0751ffb6560d54a0d5769bafc931241be4bad2e1
trungnhqse <trungnhqse@gmail.com>
2025-11-04 16:31:08 +0700

    update: init via expo

--- App.tsx
+++ App.tsx
@@ -1,46 +1,22 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import { StatusBar, useColorScheme } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from './src/screens/HomeScreen';
 import AppointmentsScreen from './src/screens/AppointmentsScreen';
 import VehiclesScreen from './src/screens/VehiclesScreen';
 import SupportScreen from './src/screens/SupportScreen';
 
-const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
-function HomeTabs() {
-  return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Appointments" component={AppointmentsScreen} />
-      <Tab.Screen name="Vehicles" component={VehiclesScreen} />
-      <Tab.Screen name="Support" component={SupportScreen} />
-    </Tab.Navigator>
-  );
-}
-
 export default function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
-    <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="Root" component={HomeTabs} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Appointments" component={AppointmentsScreen} />
+        <Tab.Screen name="Vehicles" component={VehiclesScreen} />
+        <Tab.Screen name="Support" component={SupportScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
